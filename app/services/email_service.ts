ba60ce31669841e5ccb7ecd08c7f8c4d264fcbbf
Beverly Bembeka <beverly.bembeka@iMac-de-kadea.local>
2025-01-03 15:21:30 +0100

    correction de la partie email_service avec render

--- app/services/email_service.ts
+++ app/services/email_service.ts
@@ -1,7 +1,7 @@
 import mail from "@adonisjs/mail/services/main"
 
 export default class EmailService{
-    static async send({subject,email,to,content}:{subject:string,email:string,to:string,content:string}){
+    static async send({subject,to,content}:{subject:string,email:string,to:string,content:string}){
         await mail.send((message) => {
             message
             .to(to)
