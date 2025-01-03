38348b2a2a0fc94358b33b69d71ada04f8a9bea7
Beverly Bembeka <beverly.bembeka@iMac-de-kadea.local>
2025-01-03 15:40:05 +0100

    ajout de email sur le email-services

--- app/services/email_service.ts
+++ app/services/email_service.ts
@@ -1,12 +1,14 @@
 import mail from "@adonisjs/mail/services/main"
 
-export default class EmailService{
-    static async send({subject,to,content}:{subject:string,email:string,to:string,content:string}){
-        await mail.send((message) => {
-            message
-            .to(to)
-            .subject(subject)
-            .text(content)
-        })
+export default class EmailService {
+    static async send({ subject, email, to, content }: { subject: string; email: string; to: string; content: string }) {
+      await mail.send((message) => {
+        message
+          .to(to)
+          .from(email) // Utilisation de l'email comme expéditeur
+          .subject(subject)
+          .text(content);
+      });
     }
-}
+  }
+  
